1233536b371bc9a9ee8a647c62ccb02c19b6b1ca
Carolyn Wang <carolynw@gmail.com>
2020-06-09 19:37:11 +0000

    Add a new DataServlet to delete contents of datastore.

---
+++ portfolio/src/main/java/com/google/sps/servlets/ClearServlet.java
@@ -0,0 +1,42 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet that returns some example content. TODO: modify this file to handle comments data */
+@WebServlet("/delete-data")
+public class ClearServlet extends HttpServlet {
+  private static final String COMMENT_KIND = "Comment";
+
+  /**
+   * Clear the comments.
+   */
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query(COMMENT_KIND);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    for (Entity entity : results.asIterable()) {
+      datastore.delete(entity.getKey());
+    }
+
+    // Redirect back to the proper container.
+    response.setContentType("text/html;");
+    response.getWriter().println();
+    response.sendRedirect("/index.html");
+  }
